eb53438285403dafc342e762122ff869dd997db8
hardfist <yangjianzju@gmail.com>
2025-04-29 15:36:01 +0800

    chore: make it run

---
+++ src/app.mjs
@@ -0,0 +1,7 @@
+import {createElement} from 'react';
+
+export function App(){
+    return (
+        createElement('div', null, 'Hello World')
+    );
+}
---
+++ src/index.mjs
@@ -0,0 +1,5 @@
+import { App } from './app.mjs'
+import {render} from 'react-dom';
+import * as xxx from 'react-dom';
+
+render(App(),document.getElementById('root'));
